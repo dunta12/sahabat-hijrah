5be66cb64aac82e11fe4f1e020144be3ab089c08
dunta12 <duniabercerita.info@gmail.com>
2026-08-29 07:31:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sh-v1';
+const CACHE='sh-v2';
 const ASSETS=['./','./index.html','./manifest.json','./adhan.mp3','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>Promise.all(ASSETS.map(u=>c.add(u).catch(()=>{})))));
@@ -9,11 +9,12 @@ self.addEventListener('activate',e=>{
   self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    fetch(e.request).then(res=>{
       const copy=res.clone();
       caches.open(CACHE).then(c=>c.put(e.request,copy));
       return res;
-    }).catch(()=>caches.match('./index.html')))
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
   );
 });
