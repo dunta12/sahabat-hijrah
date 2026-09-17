9139d00726e9faeff1423eb2b173481581d75ac3
dunta12 <duniabercerita.info@gmail.com>
2026-09-18 05:27:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sh-v25';
+const CACHE='sh-v26';
 const ASSETS=['./','./index.html','./en.html','./dlg.js','./iqro.html','./manifest.json','./adhan.mp3','./icon-192.png','./icon-512.png','./ScheherazadeNew-Regular.ttf'];
 
 self.addEventListener('install',e=>{
