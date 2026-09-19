ec3daf83e778f7102d7e1f70820dcd94534a4b64
dunta12 <duniabercerita.info@gmail.com>
2026-09-20 04:41:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sh-v43';
-const ASSETS=['./','./index.html','./en.html','./dlg.js','./iqro.html','./tanya.html','./manifest.json','./adhan.mp3','./icon-192.png','./icon-512.png','./ScheherazadeNew-Regular.ttf'];
+const CACHE='sh-v44';
+const ASSETS=['./','./index.html','./en.html','./dlg.js','./iqro.html','./tanya.html','./hadits.js','./manifest.json','./adhan.mp3','./icon-192.png','./icon-512.png','./ScheherazadeNew-Regular.ttf'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>Promise.all(ASSETS.map(u=>c.add(u).catch(()=>{})))));
